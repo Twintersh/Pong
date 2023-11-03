2bee2bfde9fc3930e261ce599f45818f432bbca6
Twintersh <tom.wintersheim@gmail.com>
2023-11-03 15:51:11 +0100

    add view and add task(s)

--- exo1.py
+++ exo1.py
@@ -1,24 +1,34 @@
-tasks = []
+todo_list = []
 
 def addTask(task_name):
-	tasks.append({"task": task_name, "completed": False})
+	todo_list.append({"task": task_name, "completed": False})
 	print(f'task {task_name} added!\n')
 
 def viewTasks():
-	if tasks:
-		
+	if todo_list:
+		for index, task in enumerate(todo_list):
+			status = "X" if task['completed'] else " " 
+			print(f"{index + 1}-[{status}] {task['task']}")
 	else:
 		print("the list is empty")
+	print("\n");
+
+# def completeTask()
 
 while (1):
-	print("What do you want to do? ")
-	print("1- add task ")
-	
-	choice = input("make a choice: ")
+	print("What do you want to do?")
+	print("1- add task")
+	print("2- view tasks")
+	print("5- exit")
+	choice = input("\nmake a choice: ")
 
 	if choice == "1":
-		task = input("enter a task ")
+		task = input("\nenter a task >> ")
 		addTask(task)
-	else:
-		print("à la prochaine sale dogo")
+	elif choice == "2":
+		viewTasks()
+	elif choice == "5" or choice == "exit":
+		print("See you next time!")
 		break
+	else:
+		print("Unrecognized response. Please enter a valid number.\n")
